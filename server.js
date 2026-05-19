5f6b23ae7fdd9e42c7c60831637883ec890bd6d4
bijjarapusharanya-cmyk <bijjarapusharanya@gmail.com>
2026-05-19 18:54:09 +0530

    Updated server port configuration for deployment

--- server.js
+++ server.js
@@ -314,6 +314,8 @@ app.post('/signup', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Server is running! Testing http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
 });
